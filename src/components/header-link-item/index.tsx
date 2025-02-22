f5a3c51470c4684b62518e72389249c96bd48ed5
Jacob Motta <motta_jake@Yahoo.com>
2025-02-21 21:17:43 -0800

    Added avatar to header + link adjustments

--- src/components/header-link-item/index.tsx
+++ src/components/header-link-item/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import './index.scss';
 
 export const HeaderLinkItem = ({ title, icon, active, onClick }: { title: string, icon: React.ReactNode, active: boolean, onClick: () => void }) => {
     return (
-        <div className='flex flex-col justify-center h-full px-4 hover:bg-gray-300 transition-colors duration-300 cursor-pointer' onClick={onClick}>
-
-            <div className='flex flex-row'>
+        <div className='header-link-item flex flex-col justify-center h-full px-4 cursor-pointer' onClick={onClick}>
+            <div className='flex flex-row mt-1'>
                 <div className='flex pr-2'>
                     {icon}
                 </div>
@@ -12,7 +12,7 @@ export const HeaderLinkItem = ({ title, icon, active, onClick }: { title: string
                     {title}
                 </div>
             </div>
-            <div className='flex w-full h-[2px] bg-gray-600 transition-opacity duration-300' style={{ opacity: active ? 1 : 0 }} />
+            <div className={`header-link-bar flex w-full h-[2px] bg-gray-600 opacity-0 transition-opacity duration-100 ${active ? 'opacity-100' : ''}`} />
         </div>
     );
 };
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { LuLibraryBig, LuUser } from "react-icons/lu";
 import { HeaderLinkItem } from '../header-link-item';
+import { Avatar } from 'antd';
 import colors from 'tailwindcss/colors';
+import type { MenuProps } from 'antd';
+import { Dropdown } from 'antd';
 
 const headerItems = [
   {
@@ -22,8 +25,26 @@ const headerItems = [
   },
 ]
 
+const menuItems: MenuProps['items'] = [
+  {
+    label: "Profile",
+    key: '0',
+  },
+  {
+    label: "Submit Quest",
+    key: '1',
+  },
+  {
+    label: "Logout",
+    key: '2',
+  },
+];
+
 export const Header = () => {
   const [active, setActive] = useState<number>(0);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  console.log(menuOpen);
 
   return <div className='flex flex-row h-16 w-full bg-gray-400 justify-between px-4'>
     <div className='flex flex-row items-center'>
@@ -34,10 +55,10 @@ export const Header = () => {
       }
     </div>
 
-    <div className='flex h-full items-center pr-4'>
-      <div className='flex flex-row items-center justify-center rounded-full bg-gray-500 w-10 h-10 hover:bg-gray-600 cursor-pointer duration-300'>
-        <LuUser size={20} color={colors.white} />
-      </div>
+    <div className='flex h-full items-center pr-4' >
+      <Dropdown menu={{ items: menuItems }} onOpenChange={(isOpen) => setMenuOpen(isOpen)} trigger={['click']}>
+        <Avatar size={"large"} icon={<LuUser size={20} color={colors.white} />} className="cursor-pointer hover:bg-gray-600 duration-100" style={{ backgroundColor: menuOpen ? colors.gray[600] : "" }} onClick={(e) => e?.preventDefault()} />
+      </Dropdown>
     </div>
   </div>;
 };
